6f883bb3c8cde52ea9005dfd354502650bff6b39
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-05-18 09:23:44 -0400

    initial model setups with some .all class methods

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,54 @@
 class Startup
 
+    attr_accessor :name, :domain
+    attr_reader :founder
+
+    @@all = []
+
+    def initialize(name, domain)
+        @name = name
+        @domain = domain
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
+
+
 end
+
+# `Startup#name`
+#   - returns a **string** that is the startup's name
+# - `Startup#founder`
+#   - returns a **string** that is the founder's name
+#   - Once a startup is created, the founder cannot be changed.
+# - `Startup#domain`
+#   - returns a **string** that is the startup's domain
+# - `Startup#pivot`
+#   - given a string of a **domain** and a string of a **name**, change the domain
+#     and name of the startup. This is the only public method through which the
+#     domain should be changed.
+# - `Startup.all`
+#   - should return **all** of the startup instances
+# - `Startup.find_by_founder`
+#   - given a string of a **founder's name**, returns the **first startup** whose founder's name matches
+# - `Startup.domains`
+#   - should return an **array** of all of the different startup domains
+
+
+#### Startup
+
+# - `Startup#sign_contract`
+#   - given a **venture capitalist object**, type of investment (as a string), and the amount invested (as a float), creates a new funding round and associates it with that startup and venture capitalist.
+# - `Startup#num_funding_rounds`
+#   - Returns the total number of funding rounds that the startup has gotten
+# - `Startup#total_funds`
+#   - Returns the total sum of investments that the startup has gotten
+# - `Startup#investors`
+#   - Returns a **unique** array of all the venture capitalists that have invested in this company
+# - `Startup#big_investors`
+#   - Returns a **unique** array of all the venture capitalists that have invested in this company and are in the Trés Commas club
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+startup1 = Startup.new("startup1", "www.startup1.com")
+startup2 = Startup.new("startup2", "www.startup2.com")
+startup3 = Startup.new("startup3", "www.startup3.com")
+
+ven_cap1 = VentureCapitalist.new("vencap1", "1,500,000,000")
+ven_cap2 = VentureCapitalist.new("vencap2", "2,500,000,000")
+ven_cap3 = VentureCapitalist.new("vencap3", "500,000,000")
+ven_cap4 = VentureCapitalist.new("vencap4", "100,000,000")
+
+fun_run1 = FundingRound.new(startup1, "type1", '1500000.00', ven_cap1)
+fun_run2 = FundingRound.new(startup2, "type1", "1500000.00", ven_cap2)
+fun_run3 = FundingRound.new(startup3, "type1", "1500000.00", ven_cap4)
+fun_run4 = FundingRound.new(startup4, "type1", "1500000.00", ven_cap3)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
